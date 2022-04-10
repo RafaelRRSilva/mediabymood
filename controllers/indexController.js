fbe6888a29c0a70fabfbad8a0a55e6fccd61031d
Luis <lecarcamol@gmail.com>
2022-04-10 14:49:42 -0300

    Controller importando objeto filme_aleatorio e passando para a View

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,56 +1,53 @@
-const controller = {
+const {sequelize, Filme, Humor} = require('../models');
 
-  home: (req,res) => {
-    res.render('home');
+const controller = {
+  home: (req, res) => {
+    res.render("home");
   },
 
-  primeiro_acesso: (req,res) => {
-    res.render('primeiro_acesso');
+  primeiro_acesso: (req, res) => {
+    res.render("primeiro_acesso");
   },
 
   estadoDeHumor: (req, res) => {
-    res.render('escolha_estado');
+    res.render("escolha_estado");
   },
 
-  indicacao: (req, res) => {
-    res.render('indicacao');
+  indicacao: async (req, res) => {
+
+    let filmes = await Filme.findAll()
+
+    let filme_aleatorio = filmes[0].toJSON();
+    console.log(filme_aleatorio)
+
+    res.render("indicacao", {filme_aleatorio});
   },
 
-  contato: (req,res) => {
-    res.render('contato');
+  contato: (req, res) => {
+    res.render("contato");
   },
-  como: (req,res) => {
-    res.render('comofunciona');
+  como: (req, res) => {
+    res.render("comofunciona");
   },
-form:(req,res) => {
-  res.render('formulario');
-},
+  form: (req, res) => {
+    res.render("formulario");
+  },
 
-postForm: async (req, res) => {
+  postForm: async (req, res) => {
+    const { filme } = req.body;
 
-  const { filme} = req.body;
+    const novo = await NovoFilme.findOne({
+      where: {
+        filme: filme,
+      },
+    }).catch(console.trace);
 
-  const novo = await NovoFilme.findOne({
-    where:{
-      filme:filme
-    }
-  })
-  .catch(console.trace)
+    return res.render("formulario");
 
+    req.session.novo = novo;
 
-  
-
-    return res.render('formulario');
-
-  
-
-  req.session.novo = novo;
-
-  res.redirect('/primeiro_acesso')
-}
-
-
-}
-
+    res.redirect("/primeiro_acesso");
+  },
+};
 
 module.exports = controller;
--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,72 +1,55 @@
 // const usuarios = require('../database/usuarios.json');
-const {sequelize, Usuario} = require('../models');
-const fs = require('fs');
-const { validationResult } = require('express-validator');
+const { sequelize, Usuario } = require("../models");
+const fs = require("fs");
+const { validationResult } = require("express-validator");
 
 module.exports = {
-
   cadastro: (req, res) => {
-
-    res.render('cadastro');
-
+    res.render("cadastro");
   },
 
   store: async (req, res) => {
-
     let errors = validationResult(req);
 
-    console.log('Controller funcionando')
+    console.log("Controller funcionando");
 
     if (errors.isEmpty()) {
-
-      console.log('Sem erros')
+      console.log("Sem erros");
 
       // let novoUsuario = req.body;
       // console.log(novoUsuario)
 
-      let {nome, email, senha} = req.body;
-      let novoUsuario = await Usuario.create(
-        {nome, email, senha}
-      )
+      let { nome, email, senha } = req.body;
+      let novoUsuario = await Usuario.create({ nome, email, senha });
 
-      res.redirect('/login')
-
+      res.redirect("/login");
     } else {
-
-      res.render('cadastro', {errors:errors.mapped(), old:req.body})
-
+      res.render("cadastro", { errors: errors.mapped(), old: req.body });
     }
   },
 
   showlogin: (req, res) => {
-
-    res.render('login', { error: null });
-
+    res.render("login", { error: null });
   },
 
   login: async (req, res) => {
-
     const { email, senha } = req.body;
 
     const usuario = await Usuario.findOne({
-      where:{
-        email:email,
-        senha:senha
-      }
-    })
-    .catch(console.trace)
+      where: {
+        email: email,
+        senha: senha,
+      },
+    }).catch(console.trace);
 
-  
     if (usuario === undefined) {
-
-      return res.render('login', { error: "Login/Senha inválidos" });
-
+      return res.render("login", { error: "Login/Senha inválidos" });
     }
 
     req.session.usuario = usuario;
 
-    res.redirect('/primeiro_acesso')
-  }
+    res.redirect("/primeiro_acesso");
+  },
 
   // primeiro_acesso: (req,res) => {
   //   res.render('primeiro_acesso');
@@ -79,5 +62,4 @@ module.exports = {
   // indicacao: (req, res) => {
   //   res.render('indicacao');
   // }
-
-}
+};

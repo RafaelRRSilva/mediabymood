6036397cf7ed35fa49fee90f137d50bc425ac77b
Luis <lecarcamol@gmail.com>
2022-04-24 17:55:07 -0300

    IndicaÃção gerando registro de filmes exibidos ao usuário logado

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -21,7 +21,8 @@ const controller = {
     let niv_humor = req.params.niv_humor;
 
     // Capturando info do usuario
-    console.log(req.session.usuario)
+    let infoUsuario = req.session.usuario
+    console.log(infoUsuario)
 
     // Objeto trazido do BD com id do humor passado pela URL
     let busca = await Humor.findAll({
@@ -38,13 +39,13 @@ const controller = {
     // Filtrando array de filmes segundo nível do humor
     let buscaPorNivel = filmesBusca.nivel.filter(f => f.nivel == 3)
     let filmesDoNivel = await Filme.findByPk(buscaPorNivel[0].filmes_id)
-    console.log(filmesDoNivel.toJSON())
+    //console.log(filmesDoNivel.toJSON())
 
     // Montando objeto com id e nome do humor selecionado
     let {id, nome} = filmesBusca;
     nome = nome[0].toUpperCase() + nome.slice(1);
     let infoHumor = {id, nome};
-    console.log(infoHumor)
+    //console.log(infoHumor)
 
     // Número aleatorio dentro de humor selecionado
     let num_alea = Math.floor(Math.random()* filmesBusca.filme.length)
@@ -52,6 +53,16 @@ const controller = {
     let filme_aleatorio = filmesBusca.filme[num_alea]
 
     // Gerar registro em usuarios filmes_has_usuarios
+    if(infoUsuario != undefined){
+      let filmes_id = filme_aleatorio.id;
+      let usuarios_id = infoUsuario.id;
+      console.log({filmes_id, usuarios_id, avaliacao: 0})
+      await Filmes_has_Usuarios.create({filmes_id, usuarios_id, avaliacao: 0})
+    } else {
+      console.log('Não tem usuário logado')
+    }
+
+    //let registro_filme_usuario = await Filmes_has_Usuarios.create({});
 
     res.render("indicacao", {filme_aleatorio, infoHumor});
 

868998dae79667e9f852cf27aa2fa77dafefaa25
Luis <lecarcamol@gmail.com>
2022-04-30 17:35:12 -0300

    Botão de i 'nova indicação' criado e testado

--- public/js/indicacao.js
+++ public/js/indicacao.js
@@ -1,11 +1,13 @@
 let btn_resetar = document.getElementById('btn_resetar');
+let btn_indicacao = document.getElementById('btn_indicacao')
 let idHumor = window.location.pathname.split('/')[2];
 
 console.log(btn_resetar.href)
 console.log(idHumor)
 
 window.onload = function() {
-  btn_resetar.href += idHumor
+  btn_resetar.href += idHumor;
+  btn_indicacao.href += idHumor;
   console.log(btn_resetar)
 }
 

b24968d00eccac8748ba115d773185f4814371ed
Luis <lecarcamol@gmail.com>
2022-05-06 20:14:52 -0300

    UView de login não é exibida a usuario logado

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const UsuarioNaoPrecisa = require('../middlewares/UsuarioNaoPrecisa');
 const ValidadorFormCadastro = require('../middlewares/ValidadorFormCadastro');
 // const UsuarioLogado = require('../middlewares/UsuarioLogado');
 
 router.get('/cadastro', usuarioController.cadastro);
 router.post('/cadastro', ValidadorFormCadastro, usuarioController.store);
-router.get('/login', usuarioController.showlogin);
+router.get('/login', UsuarioNaoPrecisa, usuarioController.showlogin);
 router.post('/login', usuarioController.login);
 router.get('/logout', usuarioController.logout)
 // router.get('/primeiro_acesso', UsuarioLogado, usuarioController.primeiro_acesso);
